a5e4862f04b446ad7dd03bbdb292a64c560aa576
Ivan Juarez <ivan@blindgazer.com>
2024-05-30 22:15:24 -0600

    fix: initialization and adds village type

--- rag-api/entities/article_location.py
+++ rag-api/entities/article_location.py
@@ -1,7 +1,9 @@
-import json
+from base.logger import log
+from base.serialize import SerializableDict
 
-class ArticleLocation:
-  def __init__(self, fields = {}) -> None:
+class ArticleLocation(SerializableDict):
+  def __init__(self, fields: dict = None) -> None:
+    fields = fields or {}
     self.place_id = fields.get('place_id', '')
     self.osm_type = fields.get('osm_type', '')
     self.osm_id = fields.get('osm_id', '')
@@ -11,18 +13,17 @@ class ArticleLocation:
     self.state_district = fields.get('state_district', '')
     self.county = fields.get('county', '')
     self.town = fields.get('town', '')
+    self.village = fields.get('village', '')
     self.rank_address = fields.get('rank_address', '')
     self.lat = fields.get('lat', '')
     self.lon = fields.get('lon', '')
+    super().__init__(self.__dict__)
 
   @property
-  def id(self):
+  def id(self) -> str:
     """
       place_id is nominatim's unique identifier for a location but can't be used externally as it changes each time
       database is rebuilt. Instead we use osm_type and osm_id for form a unique id. osm_id cannot be used alone
       as it is not unique across osm_types.
     """
     return f"{self.osm_type}-{self.osm_id}"
-
-  def __str__(self) -> str:
-    return json.dumps(self.__dict__)
